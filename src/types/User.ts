9c6706348e6e0833aad18269c3df0ddbf17d9406
Paulo Sergio <pauloserg861@gmail.com>
2024-07-16 22:01:01 -0300

    feat: add types

---
+++ src/types/User.ts
@@ -0,0 +1,9 @@
+export interface User {
+  _id?: string | null;
+  username: string | null;
+  email: string | null;
+  password: string | null;
+  avatar: string | null;
+  role: string | null;
+  status: string | null;
+}
