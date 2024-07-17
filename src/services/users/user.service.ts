3c3a3a685afe2ddd647183912933e9336acc84b0
Paulo Sergio <pauloserg861@gmail.com>
2024-07-16 22:00:28 -0300

    feat: implement auth service

--- src/services/users/user.service.ts
+++ src/services/users/user.service.ts
@@ -1,15 +1,17 @@
 import { axiosInstance } from "../api";
 import { Accounts } from "../../types/Accounts";
+import { AxiosResponse } from "axios";
 
 export class UserService {
-  public static async login({ email, password }: Accounts.toLogin): Promise<void> {
+  public static async login(
+    { email, password }: Accounts.toLogin
+  ): Promise<AxiosResponse<any, any>> {
     try {
-      console.log(email, password);
       const response = await axiosInstance.post("/auth/login", {
         email,
         password,
       });
-      return response?.data;
+      return response;
     } catch (error) {
       console.error("Failed to login", error);
       throw error;
